7543454bf61cf082427f4a6a0af6c56da13f89cc
harsha kukreja <harsha.kukreja@hpe.com>
2017-02-06 18:28:16 +0530

    DeviceStatus

--- app/src/main/java/fragments/DeviceStatus.java
+++ app/src/main/java/fragments/DeviceStatus.java
@@ -3,9 +3,15 @@ package fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import example.com.industrialautomation.R;
 
@@ -14,6 +20,8 @@ import example.com.industrialautomation.R;
  * A simple {@link Fragment} subclass.
  */
 public class DeviceStatus extends Fragment {
+    private RecyclerView recyclerView;
+    LinearLayout linearDetail,linearRealDevices;
 
     private View v;
 
@@ -28,8 +36,8 @@ public class DeviceStatus extends Fragment {
         // Inflate the layout for this fragment
         v= inflater.inflate(R.layout.fragment_device_status, container, false);
 
-
         return v;
     }
+
 
 }
