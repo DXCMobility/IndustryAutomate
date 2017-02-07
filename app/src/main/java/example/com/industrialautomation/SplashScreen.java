06e5bd6372c806358b4a4f7d2de7abc82091395f
Prashant Yadav <prashant.yadav@hpe.com>
2017-02-08 00:32:12 +0530

    adding Images in drawable

--- app/src/main/java/example/com/industrialautomation/SplashScreen.java
+++ app/src/main/java/example/com/industrialautomation/SplashScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import example.com.industrialautomation.activities.MainActivity;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
