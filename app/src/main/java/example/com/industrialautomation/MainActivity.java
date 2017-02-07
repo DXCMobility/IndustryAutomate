51d45e69afdcd46eeaeb5e961a7519acf4ee4c46
ashu <ashumku7@gmail.com>
2017-02-07 15:20:21 +0530

    MainActivity, space is removed

--- app/src/main/java/example/com/industrialautomation/MainActivity.java
+++ app/src/main/java/example/com/industrialautomation/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-
 import fragments.Dashboard;
 import fragments.DeviceStatus;
 import fragments.Feedback;
